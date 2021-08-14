e917662eb6472e4fb9ee71415ec2b7516be88b50
马灼堂 <donlu@qq.com>
2021-08-14 23:03:05 +0800

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -23,8 +23,8 @@ if ls ./.lego/certificates | grep "$domain"
     then
     echo '证书签发成功'
     mkdir /root/.cert
-    cp ./.lego/certificates/$domain.crt /root/.cert/server.crt
-    cp ./.lego/certificates/$domain.key /root/.cert/server.key
+    cp ./.lego/certificates/$domain.crt /root/cert/cert.crt
+    cp ./.lego/certificates/$domain.key /root/cert/private.key
 else
     echo '证书签发失败'
 fi
